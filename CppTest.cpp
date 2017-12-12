0774ca93185b0a5d576a8557b01b244f5acf3cc3
EricZ <onion55zh@gmail.com>
2017-12-12 13:26:12 +0000

    initial commit

---
+++ CppTest.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern "C" 
+{
+    #include "lua.h"
+    #include "lualib.h"
+    #include "lauxlib"
+}
+
+int main (int argc, char *argv[])
+{
+
+
+    return EXIT_SUCCESS;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+*This is a project for testing Lua and C++ bindings.
+
+I will be calling a C++ function in lua and a Lua function in C++.

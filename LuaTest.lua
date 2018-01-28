e531f8d738be2755b27fd77176e509650019e609
RaykoEZ <onion55zh@gmail.com>
2018-01-28 22:58:32 +0000

    Starting Demo Implementation

---
+++ LuaTest.lua
@@ -0,0 +1,14 @@
+print("hello world.")
+
+-- defines a factorial function
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n-1) -- recursion boys!
+    end
+    end
+    
+print("enter a number:")
+a = io.read("*number")        -- read a number
+print(fact(a))

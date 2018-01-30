e6f1c2e47f5dbb53a29bad940116d5275d15ad13
EricZ <onion55zh@gmail.com>
2018-01-30 14:45:31 +0000

    implementing test lua and cpp

--- CppTest.cpp
+++ CppTest.cpp
@@ -7,9 +7,30 @@ extern "C"
     #include "lauxlib.h"
 }
 
-int main (int argc, char *argv[])
+//doing something
+int luaFactorial( lua_state* _state, int _num)
+{   
+    int ret;
+
+    lua_getglobal(_state, "fact");
+
+    return ret;
+}
+
+void initLua()
 {
 
+}
+
+int main (int argc, char *argv[])
+{
+    //declare Lua State
+    lua_State* state1 = luaL_newstate();
+    //init Lua state
+    state1 = lua_open();
+
+    //exit Lua State
+    if(state1) lua_close(state1);
 
     return EXIT_SUCCESS;
 }
